ffc65f60bfa8232326379d15023faac4274535cc
ericzha <ericzha@FatHp10.com>
2023-09-23 15:04:22 +0800

    加入build.sh 运行前拷贝库到输出路径

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+set -x
+CurrentPath=$(pwd)
+echo copy lib start
+
+mkdir -p test_tool/test_win/x64/Release
+mkdir -p test_tool/test_win/x64/Debug
+cp lib/win64/*   test_tool/test_win/x64/Release
+cp lib/win64_debug/*   test_tool/test_win/x64/Debug
+
+echo copy lib end
+
+echo end
